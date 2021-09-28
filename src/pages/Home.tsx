ae69a71b93c1b59f4c5bb3e8b4c6859c41854c54
Renato Gonçalves <renatoslip@hotmail.com>
2021-09-28 13:38:20 -0300

    stylized authentication screen

---
+++ src/pages/Home.tsx
@@ -0,0 +1,38 @@
+import IllustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
+
+// Components
+import { Button } from "../components/Button";
+
+// Style
+import "../styles/auth.scss";
+
+export function Home() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={IllustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie salas de Q&A ao-vivo</strong>
+        <p>Tire suas dúvidas de sua audiência em tempo real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <button className="create-room">
+            <img src={googleIconImg} alt="Logo do Googlee" />
+            Crie sua sala com o Google
+          </button>
+          <div className="separator">ou entre em uma sala</div>
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na sala</Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
